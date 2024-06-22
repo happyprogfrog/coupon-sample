6a6d957b7a732715a3c1f7f62557ab6c31bffcf7
happyprogfrog <happyprogfrog@gmail.com>
2024-06-22 23:59:23 +0900

    프로젝트 생성

---
+++ src/main/java/me/progfrog/coupon_sample/CouponSampleApplication.java
@@ -0,0 +1,13 @@
+package me.progfrog.coupon_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/progfrog/coupon_sample/CouponSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package me.progfrog.coupon_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
