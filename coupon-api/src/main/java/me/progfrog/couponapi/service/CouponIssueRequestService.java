ee92d2f8b00e17241c0c1fb8e6951c1b33715535
happyprogfrog <happyprogfrog@gmail.com>
2024-06-24 15:39:26 +0900

    feat: 쿠폰 발급 API 개발

---
+++ coupon-api/src/main/java/me/progfrog/couponapi/service/CouponIssueRequestService.java
@@ -0,0 +1,20 @@
+package me.progfrog.couponapi.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import me.progfrog.couponapi.dto.CouponIssueRequestDto;
+import me.progfrog.couponcore.service.CouponIssueService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+public class CouponIssueRequestService {
+
+    private final CouponIssueService couponIssueService;
+
+    public void issueRequestV1(CouponIssueRequestDto requestDto) {
+        couponIssueService.issue(requestDto.couponId(), requestDto.userId());
+        log.info("쿠폰 발급 완료! coupon_id: %s, user_id: %s".formatted(requestDto.couponId(), requestDto.userId()));
+    }
+}
