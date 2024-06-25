dd9b3ff9296fb93bb23a28cabd73da66e9d04fa4
happyprogfrog <happyprogfrog@gmail.com>
2024-06-25 12:30:09 +0900

    feat: redis를 사용한 분산락 구현
    - synchronized 키워드는 자바 애플리케이션에 종속되며, 여러 서버로 확장이 되는 순간 락을 제대로 관리할 수 없다.
    - 분산락 구현을 통해 동시성 이슈를 제어한다.

--- coupon-api/src/main/java/me/progfrog/couponapi/service/CouponIssueRequestService.java
+++ coupon-api/src/main/java/me/progfrog/couponapi/service/CouponIssueRequestService.java
@@ -3,6 +3,7 @@ package me.progfrog.couponapi.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.progfrog.couponapi.dto.CouponIssueRequestDto;
+import me.progfrog.couponcore.component.DistributeLockExecutor;
 import me.progfrog.couponcore.service.CouponIssueService;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +13,12 @@ import org.springframework.stereotype.Service;
 public class CouponIssueRequestService {
 
     private final CouponIssueService couponIssueService;
+    private final DistributeLockExecutor distributeLockExecutor;
 
     public void issueRequestV1(CouponIssueRequestDto requestDto) {
-        synchronized (this) {
+        distributeLockExecutor.execute("lock_" + requestDto.couponId(), 10000, 10000, () -> {
             couponIssueService.issue(requestDto.couponId(), requestDto.userId());
-        }
+        });
         log.info("쿠폰 발급 완료! coupon_id: %s, user_id: %s".formatted(requestDto.couponId(), requestDto.userId()));
     }
 }
