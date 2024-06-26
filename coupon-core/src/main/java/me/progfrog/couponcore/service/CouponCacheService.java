0a70858d713d2bdcda370bade29919a23e25d118
happyprogfrog <happyprogfrog@gmail.com>
2024-06-26 16:21:45 +0900

    refactor: 쿠폰 정보를 레디스에서 읽어오는 부분을 로컬 캐시를 통해 조금 더 개선
    - 애플리케이션 메모리이므로, 레디스에 붙기 위해 사용되는 네트워크 시간 등을 절약할 수 있다.
    - AOP를 사용하기 위해서는 프록시 객체를 사용해야하는데, 클래스 내에서 자기 자신의 메서드를 호출할 때는 프록시 객체를 통하지 않기 때문에 프록시 객체를 사용하기 위해 별도의 애노테이션을 추가하고 관련 코드를 추가해준다.

--- coupon-core/src/main/java/me/progfrog/couponcore/service/CouponCacheService.java
+++ coupon-core/src/main/java/me/progfrog/couponcore/service/CouponCacheService.java
@@ -3,6 +3,7 @@ package me.progfrog.couponcore.service;
 import lombok.RequiredArgsConstructor;
 import me.progfrog.couponcore.model.Coupon;
 import me.progfrog.couponcore.repository.redis.dto.CouponRedisEntity;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +17,14 @@ public class CouponCacheService {
     public CouponRedisEntity getCouponCache(long couponId) {
         Coupon coupon = couponIssueService.findCoupon(couponId);
         return new CouponRedisEntity(coupon);
+    }
+
+    @Cacheable(cacheNames = "coupon", cacheManager = "localCacheManager")
+    public CouponRedisEntity getCouponLocalCache(long couponId) {
+        return proxy().getCouponCache(couponId);
+    }
+
+    private CouponCacheService proxy() {
+        return ((CouponCacheService) AopContext.currentProxy());
     }
 }
