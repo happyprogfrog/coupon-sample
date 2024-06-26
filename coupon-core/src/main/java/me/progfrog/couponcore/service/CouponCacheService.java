967a5abc0b654ee39512722c9d34b7dc4fac4deb
happyprogfrog <happyprogfrog@gmail.com>
2024-06-26 17:03:28 +0900

    refactor: 쿠폰 발급이 완료 되었으면, 캐시를 지우고 발급 불가능 플래그를 켜서 더 이상 쿠폰 발급 요청 로직을 타지 않도록 개선
    - 쿠폰 발급 트랜잭션 로직 수행 시, 쿠폰 발급이 완료 되었으면 이벤트 발생
    - 해당 이벤트를 감지하고 하고 있다가 이벤트가 발생되면 캐시를 업데이트
    - 다시 캐시에 쿠폰 정보를 저장하는 과정에서 발급 불가능 플래그가 켜짐
    - 이후 유저 요청에 대해서는 더 이상 쿠폰 발급 요청 로직을 타지 않게되면서 RPS가 높아짐

--- coupon-core/src/main/java/me/progfrog/couponcore/service/CouponCacheService.java
+++ coupon-core/src/main/java/me/progfrog/couponcore/service/CouponCacheService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import me.progfrog.couponcore.model.Coupon;
 import me.progfrog.couponcore.repository.redis.dto.CouponRedisEntity;
 import org.springframework.aop.framework.AopContext;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -22,6 +23,16 @@ public class CouponCacheService {
     @Cacheable(cacheNames = "coupon", cacheManager = "localCacheManager")
     public CouponRedisEntity getCouponLocalCache(long couponId) {
         return proxy().getCouponCache(couponId);
+    }
+
+    @CachePut(cacheNames = "coupon")
+    public CouponRedisEntity putCouponCache(long couponId) {
+        return getCouponCache(couponId);
+    }
+
+    @CachePut(cacheNames = "coupon", cacheManager = "localCacheManager")
+    public CouponRedisEntity putCouponLocalCache(long couponId) {
+        return getCouponLocalCache(couponId);
     }
 
     private CouponCacheService proxy() {
