6435561e7a0ee5a298f5bdb392ecbfc34215b0c1
happyprogfrog <happyprogfrog@gmail.com>
2024-06-26 10:07:14 +0900

    refactor: redis 캐시 사용 적용
    - 쿠폰 존재 검증
    - 쿠폰 발급 유효 기간 검증
    - 수량 조회 및 중복 발급 여부 검증 코드 위치 변경
    - LocalDateTime 직렬화, 역직렬화를 위한 의존성 및 애노테이션 추가

---
+++ coupon-core/src/main/java/me/progfrog/couponcore/repository/redis/dto/CouponRedisEntity.java
@@ -0,0 +1,48 @@
+package me.progfrog.couponcore.repository.redis.dto;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import me.progfrog.couponcore.model.Coupon;
+import me.progfrog.couponcore.model.CouponType;
+
+import java.time.LocalDateTime;
+
+import static me.progfrog.couponcore.exception.ErrorCode.INVALID_COUPON_ISSUE_DATE;
+
+public record CouponRedisEntity(
+        Long id,
+        CouponType couponType,
+        Integer totalQuantity,
+
+        @JsonSerialize(using = LocalDateTimeSerializer.class)
+        @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+        LocalDateTime dateIssueStart,
+
+        @JsonSerialize(using = LocalDateTimeSerializer.class)
+        @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+        LocalDateTime dateIssueEnd
+) {
+
+    public CouponRedisEntity(Coupon coupon) {
+        this(
+                coupon.getId(),
+                coupon.getCouponType(),
+                coupon.getTotalQuantity(),
+                coupon.getDateIssueStart(),
+                coupon.getDateIssueEnd()
+        );
+    }
+
+    private boolean isAvailableIssueDate() {
+        LocalDateTime now = LocalDateTime.now();
+        return dateIssueStart.isBefore(now) && dateIssueEnd.isAfter(now);
+    }
+
+    public void checkIssuableCoupon() {
+        if (!isAvailableIssueDate()) {
+            throw INVALID_COUPON_ISSUE_DATE.build(LocalDateTime.now(), dateIssueStart, dateIssueStart);
+        }
+    }
+}
--- coupon-core/src/main/java/me/progfrog/couponcore/service/AsyncCouponIssueServiceV1.java
+++ coupon-core/src/main/java/me/progfrog/couponcore/service/AsyncCouponIssueServiceV1.java
@@ -4,14 +4,12 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import me.progfrog.couponcore.component.DistributeLockExecutor;
-import me.progfrog.couponcore.model.Coupon;
 import me.progfrog.couponcore.repository.redis.RedisRepository;
 import me.progfrog.couponcore.repository.redis.dto.CouponPushQueueRequest;
+import me.progfrog.couponcore.repository.redis.dto.CouponRedisEntity;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-
-import static me.progfrog.couponcore.exception.ErrorCode.*;
+import static me.progfrog.couponcore.exception.ErrorCode.FAIL_COUPON_PUSH_QUEUE_REQUEST;
 import static me.progfrog.couponcore.util.CouponRedisUtils.getIssueRequestKey;
 import static me.progfrog.couponcore.util.CouponRedisUtils.getIssueRequestQueue;
 
@@ -21,31 +19,16 @@ public class AsyncCouponIssueServiceV1 {
 
     private final RedisRepository redisRepository;
     private final CouponIssueRedisService couponIssueRedisService;
-    private final CouponIssueService couponIssueService;
     private final DistributeLockExecutor distributeLockExecutor;
+    private final CouponCacheService couponCacheService;
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     public void issue(long couponId, long userId) {
-        // 1. 쿠폰 존재 확인
-        Coupon coupon = couponIssueService.findCoupon(couponId);
-
+        CouponRedisEntity coupon = couponCacheService.getCouponCache(couponId);
+        coupon.checkIssuableCoupon();
         distributeLockExecutor.execute("coupon_issue_lock_%s".formatted(couponId), 3000, 3000, () -> {
-            // 2. 날짜 조회
-            if (!coupon.isAvailableIssueDate()) {
-                throw INVALID_COUPON_ISSUE_DATE.build(LocalDateTime.now(), coupon.getDateIssueStart(), coupon.getDateIssueEnd());
-            }
-            // 3. 수량 조회
-            if (!couponIssueRedisService.isAvailableTotalIssueQuantity(coupon.getTotalQuantity(), couponId)) {
-                throw INVALID_COUPON_ISSUE_QUANTITY.build();
-            }
-
-            // 4. 중복 발급 여부 확인
-            if (!couponIssueRedisService.isAvailableUserIssueQuantity(couponId, userId)) {
-                throw DUPLICATED_COUPON_ISSUE.build(couponId, userId);
-            }
-
-            // 5. 요청 추가 및 쿠폰 발급 큐에 적재
+            couponIssueRedisService.checkCouponIssueQuantity(coupon, userId);
             issueRequest(couponId, userId);
         });
     }
@@ -55,7 +38,6 @@ public class AsyncCouponIssueServiceV1 {
         try {
             // 요청 추가
             redisRepository.sAdd(getIssueRequestKey(couponId), String.valueOf(userId));
-
             // 쿠폰 발급 큐에 적재
             String value = objectMapper.writeValueAsString(pushQueueRequest);
             redisRepository.rPush(getIssueRequestQueue(), value);
