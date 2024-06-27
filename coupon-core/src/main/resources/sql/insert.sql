8afb8cbda3353c683640f93ce070504c2f43d3c0
happyprogfrog <happyprogfrog@gmail.com>
2024-06-27 18:36:04 +0900

    docs: DB 테이블 쿼리 파일 수정

--- coupon-core/src/main/resources/sql/insert.sql
+++ coupon-core/src/main/resources/sql/insert.sql
@@ -1,3 +1,3 @@
 INSERT INTO coupons
 (title, coupon_type, total_quantity, issued_quantity, discount_amount, min_available_amount, date_issue_start, date_issue_end, date_created, date_updated)
-VALUES ('인생은 선착순' , 'FISRT_COME_FIRST_SERVED', 500, 0, 100000, 110000, "2024-06-23 12:00:00", "2024-06-26 12:00:00", NOW(), NOW());
+VALUES ('인생은 선착순' , 'FIRST_COME_FIRST_SERVED', 500, 0, 100000, 110000, "2024-06-23 12:00:00", "2024-06-26 12:00:00", NOW(), NOW());
